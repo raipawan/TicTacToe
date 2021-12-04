83961901e36de0ae72502a7ce8068b6bc122a410
pavan kumar <raipawan337@gmail.com>
2021-12-04 10:11:37 +0530

    added UC2

--- TicTacToe/TicTacToe/UC2-ChooseLetter.cs
+++ TicTacToe/TicTacToe/UC2-ChooseLetter.cs
@@ -6,5 +6,14 @@ namespace TicTacToe
 {
     class UC2_ChooseLetter
     {
+        public static char Input()
+        {
+
+            Console.WriteLine("choose your letter ");
+            char input = Convert.ToChar(Console.ReadLine());
+            char player = (input == 'X') ? 'X' : 'O';
+            return player;
+
+        }
     }
 }
